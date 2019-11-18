eb4673ae7fcbab58cd1cb7944e6f16e2289ce587
Robert Paul <rob.k.paul@gmail.com>
2019-11-18 11:41:19 -0800

    working on getting #s from calc buttons

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,14 @@
-const button = document.querySelector('.button-container--one');
-let num = 0;
-button.addEventListener('mouseover', function (event){
-  console.log(num);
-  num++;
+const button = document.querySelector('.ui-container');
+let numOne = 0;
+let numTwo = 0;
+button.addEventListener('click', function (event){
+    const buttonPressed = event.target.name;
+    const currentNumber = parseInt(event.target.innerText, 10);
+    if(isNaN(currentNumber)){
+      console.log(buttonPressed);
+    }
+    else{
+      console.log(currentNumber);
+    }
+    document.querySelector(".input-window--text").innerText = "yeet";
 });
